2ce69c67dfd6ade3daeec9d6053e4481dd8e2688
Chen <18842691097@163.com>
2018-08-07 18:19:41 +0800

    完善 增加跳转帖子详情功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,5 @@
 // pages/home.js
-import getInvitationList from '../../common/js/services.js'
+import {getInvitationList} from '../../common/js/services.js'
 
 Page({
 
@@ -9,14 +9,15 @@ Page({
   data: {
     invitationLists: [],
     defaultAvatarUrl: '../../assets/common/male_pic@3x.png',
-    defaultNickName: 'xueyuan'
+    defaultNickName: 'xueyuan',
+    pageNo: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this._getInvitationlist()
+    this._getInvitationlist(this.data.pageNo)
   },
 
   /**
@@ -58,7 +59,9 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    this.data.pageNo++
+    console.log(this.data.pageNo)
+    this._getInvitationlist(this.data.pageNo)
   },
 
   /**
@@ -67,35 +70,45 @@ Page({
   onShareAppMessage: function () {
   
   },
-  _getInvitationlist: function () {
-    // getInvitationList(10, this.data.invitationLists)
-    // .then(function (res) {
-    //   this.data.invitationLists = res
-    // })
-    // .catch(err => {
-     
-    // })
+  _getInvitationlist: function (pageNo) {
     let that = this
-    wx.request({
-      url: 'http://36.110.107.219:20002/soekao-web-dist/getcha',
-      data: {
-        content: 'zone_topics_ex',
-        page_no: 1,
-        page_size: 10,
-        account: '17610992252'
-      },
-      method: 'GET',
-      success: function(res) {
-        console.log(res)
-        that.setData({
-          invitationLists: res.data
-        })
-        console.log(that.data.invitationLists)
-      },
-      fail: function(err) {
-        console.log(err)
-
-      }
+    console.log(getInvitationList(pageNo))
+    getInvitationList(pageNo)
+    .then(res => {
+      console.log(res)
+      that.setData({
+        invitationLists: that.data.invitationLists.concat(res)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      wx.showToast({
+        title: '网络错误2',
+      })
+    })
+  },
+  toInvitationDetail: function (event) {
+    let dataset = event.currentTarget.dataset
+    let createTime = dataset.createTime
+    let body = dataset.body
+    console.log(createTime)
+    wx.navigateTo({
+      url: `../invitationDetail/invitationDetail?createTime=${createTime}&body=${body}`
+    })
+  },
+  viewPic: function (event) {
+    let that = this
+    console.log(event.currentTarget.dataset)
+    let dataset = event.currentTarget.dataset
+    let picArr = dataset.picArr
+    let index = dataset.index
+    let tempArr = []
+    for (let item of picArr) {
+      tempArr.push(`http://36.110.107.219:20002/soekao-web-dist/getcha?content=zone_attach&key=${item.attach_id}&compress=1&account=17610992252&callback=_jp0`)
+    }
+    wx.previewImage({
+      current: tempArr[index],
+      urls: tempArr
     })
   }
 })
